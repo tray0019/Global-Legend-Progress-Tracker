d4a96d46087631dd9e4c97fd73d42dd87ee899e4
tray0019 <tray0019@algonquinlive.com>
2025-11-16 23:29:24 -0500

    Added springboot dependency

---
+++ Backend/src/main/java/com/GLPT/Lesgo/LesgoApplication.java
@@ -0,0 +1,13 @@
+package com.GLPT.Lesgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesgoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/GLPT/Lesgo/LesgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GLPT.Lesgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
