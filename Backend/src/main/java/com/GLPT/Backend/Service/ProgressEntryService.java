efe181e6f154d5d1f0b9093c2d06078e323f93a1
tray0019 <tray0019@algonquinlive.com>
2025-11-17 23:15:31 -0500

    Added Crud for ProgressEntry Service class. The method addEntryTOGoal take the goal id and add entry to the Goal

--- Backend/src/main/java/com/GLPT/Backend/Service/ProgressEntryService.java
+++ Backend/src/main/java/com/GLPT/Backend/Service/ProgressEntryService.java
@@ -1,18 +1,75 @@
 package com.GLPT.Backend.Service;
 
+import com.GLPT.Backend.Entity.Goal;
+import com.GLPT.Backend.Entity.ProgressEntry;
+import com.GLPT.Backend.Repository.GoalRepository;
 import com.GLPT.Backend.Repository.ProgressEntryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProgressEntryService {
 
     private final ProgressEntryRepository repo;
+    private final GoalRepository goalRepo;
 
-    ProgressEntryService(ProgressEntryRepository repo){
+    ProgressEntryService(ProgressEntryRepository repo, GoalRepository goalRepo){
         this.repo = repo;
+        this.goalRepo = goalRepo;
     }
 
-    //
+    /**
+     * Create entry description for a Goal
+     */
+    public ProgressEntry addEntryToGoal(long goalId, String description){
+
+        Goal goal = goalRepo.findById(goalId)
+                .orElseThrow(() -> new RuntimeException("Goal not found"));
+
+        ProgressEntry entry = new ProgressEntry();
+        entry.setDescription(description);
+
+        entry.setGoal(goal);
+
+        return repo.save(entry);
+    }
+
+    /**
+     * -- View all progress description
+     *  for a Goal
+     */
+    public List<ProgressEntry> getAllEntries(){
+        return repo.findAll();
+    }
+
+
+    /**
+     * -- Renamed the entry Description
+     *  if not found throw exception
+     */
+    public ProgressEntry updateDescription(long entryId, String  newEntryDescription){
+            ProgressEntry entry = repo.findById(entryId)
+                    .orElseThrow(() -> new RuntimeException("Entry not found"));
+
+            entry.setDescription(newEntryDescription);
+            return repo.save(entry);
+
+    }
+
+    /**
+     * -- Delete an entry by id
+     */
+    public void deleteDescription(long entryId){
+
+        if(!repo.existsById(entryId)){
+            throw new RuntimeException("Entry not found");
+        }
+        repo.deleteById(entryId);
+    }
+
+
 
 
 
