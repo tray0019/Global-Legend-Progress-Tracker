b98d13850dbab649bc151a34b0f00ea90052dca5
tray0019 <tray0019@algonquinlive.com>
2025-11-18 19:35:37 -0500

    Set up my Goal RestController - createGoal(), getAllGOal(),getGoal(), renamedGoal(), deleteGoal()

---
+++ Backend/src/main/java/com/GLPT/Backend/Controller/GoalController.java
@@ -0,0 +1,62 @@
+package com.GLPT.Backend.Controller;
+
+
+import com.GLPT.Backend.Entity.Goal;
+import com.GLPT.Backend.Service.GoalService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GoalController {
+
+
+    private final GoalService service;
+
+    GoalController(GoalService service){
+        this.service = service;
+    }
+
+    @PostMapping("/goals")
+    public Goal createGoal(@RequestBody Goal goal){
+        return service.createNewGoal(goal);
+    }
+
+    /**
+     * -- View All Goals
+     */
+    @GetMapping("/goals")//set goalView
+    public List<Goal> getAllGoal(){
+        return service.viewAllGoal();
+    }
+
+    /**
+     *  -- View One Goal and its Progress EntryService
+     */
+
+    @GetMapping("/goals/{goalId}")
+    public Goal getGoal(@PathVariable long goalId){
+        return service.viewGoal(goalId);
+    }
+
+    /**
+     *  -- Rename Goal title
+     */
+    @PutMapping("/goals/{goalId}")
+    public Goal renameGoal(@PathVariable long goalId, @RequestParam String newTitle){
+        return service.renameGoal(goalId,newTitle);
+    }
+
+    /**
+     * -- delete a goal
+     */
+    @DeleteMapping("/goals/{goalId}")
+    public void deleteGoal(@PathVariable long goalId){
+         service.deleteGoal(goalId);
+    }
+
+
+
+
+
+}
---
+++ Backend/src/main/java/com/GLPT/Backend/Controller/ProgressEntryController.java
@@ -0,0 +1,8 @@
+package com.GLPT.Backend.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProgressEntryController {
+
+}
