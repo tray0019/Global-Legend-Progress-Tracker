951dbbb796d6ca83bbe6c4577350770d93a7bd83
tray0019 <tray0019@algonquinlive.com>
2025-11-19 16:24:08 -0500

    Created DTO for creating Goal and its process

---
+++ Backend/src/main/java/com/GLPT/Backend/DTO/GoalProcessDto.java
@@ -0,0 +1,13 @@
+package com.GLPT.Backend.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GoalProcessDto {
+
+    private long Id;
+    private String goalTitle;
+
+}
