e078efebe9ac3b356899d67edfe7a0d4c26f5a32
tray0019 <tray0019@algonquinlive.com>
2025-12-01 13:41:38 -0500

    Added Goal check repository with  finByGoalAndCheckDate and fintByGoalAndCheckDateBetween abstract method.

--- Backend/src/main/java/com/GLPT/Backend/Repository/GoalCheckRepository.java
+++ Backend/src/main/java/com/GLPT/Backend/Repository/GoalCheckRepository.java
@@ -1,9 +1,18 @@
 package com.GLPT.Backend.Repository;
 
+import com.GLPT.Backend.Entity.Goal;
 import com.GLPT.Backend.Entity.GoalCheck;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface GoalCheckRepository extends JpaRepository<GoalCheck, Long> {
+
+        Optional<GoalCheck> findByGoalAndCheckDate(Goal goal, LocalDate checkDate);
+        List<GoalCheck> findByGoalAndCheckDateBetween(Goal goal, LocalDate from, LocalDate to);
+
 }
