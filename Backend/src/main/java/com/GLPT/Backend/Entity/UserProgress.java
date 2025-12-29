dfce4b57eb26ba75ce2104e17d2679daa6a41a76
tray0019 <tray0019@algonquinlive.com>
2025-12-29 07:37:04 -0500

    Added UserProgress entity. This entity takes in id, totalXP, currenRank defaukl bronze, lastActitivtyDate, createdAt and updateAt. Also methods like onUpdate and lombok data

---
+++ Backend/src/main/java/com/GLPT/Backend/Entity/UserProgress.java
@@ -0,0 +1,45 @@
+package com.GLPT.Backend.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="user_progress")
+@Data
+public class UserProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int totalXP = 0;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Rank currentRank = Rank.BRONZE;
+
+    @Column(nullable = false)
+    private LocalDate lastActivityDate;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updateAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updateAt =  LocalDateTime.now();
+        this.lastActivityDate = LocalDate.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updateAt = LocalDateTime.now();
+    }
+
+}
