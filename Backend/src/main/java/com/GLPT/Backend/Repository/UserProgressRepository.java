14796ba9655aba956306111e45d28a6f2e433532
tray0019 <tray0019@algonquinlive.com>
2025-12-29 22:11:05 -0500

    fix userPorgress repository problem

--- Backend/src/main/java/com/GLPT/Backend/Repository/UserProgressRepository.java
+++ Backend/src/main/java/com/GLPT/Backend/Repository/UserProgressRepository.java
@@ -2,7 +2,9 @@ package com.GLPT.Backend.Repository;
 
 import com.GLPT.Backend.Entity.UserProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserProgressRepository extends JpaRepository<UserProgress, Long> {
-    UserProgress findTopByOrderIdAsc();
+    UserProgress findTopByOrderByIdAsc();
 }
