01bc9326773e888df1c39cdd4a01498c04e8dc4f
tray0019 <tray0019@algonquinlive.com>
2026-01-10 21:12:58 -0500

    ADded complete profile method in controller class

--- Backend/src/main/java/com/GLPT/Backend/Controller/UserController.java
+++ Backend/src/main/java/com/GLPT/Backend/Controller/UserController.java
@@ -1,15 +1,14 @@
 package com.GLPT.Backend.Controller;
 
+import com.GLPT.Backend.DTO.CompleteProfileRequest;
 import com.GLPT.Backend.DTO.UserRegistrationRequest;
 import com.GLPT.Backend.DTO.UserResponse;
 import com.GLPT.Backend.Entity.User;
 import com.GLPT.Backend.Service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -30,6 +29,25 @@ public class UserController {
                 user.getGender(),
                 user.isProfileCompleted()
         );
+    }
+
+    @PostMapping("/complete-profile/{userId}")
+    public UserResponse completeProfile(
+            @PathVariable Long userId,
+            @Valid @RequestBody CompleteProfileRequest request){
+
+        User user = userService.completeProfile(userId, request);
+
+        return new UserResponse(
+          user.getId(),
+          user.getFirstName(),
+          user.getLastName(),
+          user.getEmail(),
+          user.getBirthDate(),
+          user.getGender(),
+          user.isProfileCompleted()
+        );
+
     }
 
 }
