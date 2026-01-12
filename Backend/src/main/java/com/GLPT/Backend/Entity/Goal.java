3298a84769a39a21085a2eb297bc14a448617b02
tray0019 <tray0019@algonquinlive.com>
2026-01-11 23:04:05 -0500

    Update Goal user attributes and set nullable = false. Each goal must belong to one user.

--- Backend/src/main/java/com/GLPT/Backend/Entity/Goal.java
+++ Backend/src/main/java/com/GLPT/Backend/Entity/Goal.java
@@ -40,7 +40,7 @@ public class Goal {
     private boolean isAchievement;
 
     @ManyToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name="user_id", nullable = false)
     private User user;
 
 }
