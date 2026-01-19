a5da1dae70d5ceedc7aee4b39f2749d42cc64e23
tray0019 <tray0019@algonquinlive.com>
2026-01-19 06:53:37 -0500

    ADded userGoalCheckApi and implemented some at UserHome

--- frontend/src/pages/UserHome.jsx
+++ frontend/src/pages/UserHome.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { getUserGoals } from '../api/userGoalApi';
+
 import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
 import { reorderGoals } from '../api/goalApi';
 import UserGoalCard from '../usercomponents/UserGoalCard';
@@ -7,6 +8,25 @@ import UserGoalCard from '../usercomponents/UserGoalCard';
 function UserHome({ currentUser }) {
   const [goals, setGoals] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [isLoadingGoalDetails, setIsLoadingGoalDetails] = useState([]);
+
+  const loadingGoalLock = useRef({});
+
+  function pad2(number) {
+    return number < 10 ? '0' + number : String(number);
+  }
+
+  function getCurrentMonthRange() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+
+    const from = `${year}-${pad2(month + 1)}-01`;
+    const lastDay = new Date(year, month + 1, 0).getDate();
+    const to = `${year}-${pad2(month + 1)}-${pad2(lastDay)}`;
+
+    return { from, to };
+  }
 
   useEffect(() => {
     if (!currentUser) return;
@@ -24,6 +44,38 @@ function UserHome({ currentUser }) {
 
     fetchGoals();
   }, [currentUser]);
+
+  /* ---------- LOAD ONE GOAL DETAILS + CHECKMARKS ---------- */
+  const loadSelectedGoalAndChecks = async (goalId, monthRange = null) => {
+    if (loadingGoalLock.current[goalId]) return;
+    loadingGoalLock.current[goalId] = true;
+
+    setIsLoadingGoalDetails(true);
+
+    try {
+      if (!monthRange) monthRange = getCurrentMonthRange();
+
+      const [goalRes, checksRes] = await Promise.all([
+        getGoalById(goalId), // entries
+        getGoalChecks(goalId, monthRange.from, monthRange.to), // checkmarks
+      ]);
+
+      setGoalDetails((prev) => ({
+        ...prev,
+        [goalId]: goalRes.data, // make sure entries are updated
+      }));
+
+      setGoalCheckDates((prev) => ({
+        ...prev,
+        [goalId]: checksRes.data.map((item) => item.date),
+      }));
+    } catch (err) {
+      console.error('Error loading goal details:', err);
+    } finally {
+      setIsLoadingGoalDetails(false);
+      loadingGoalLock.current[goalId] = false;
+    }
+  };
 
   /* ---------- DRAG AND DROP SORTING ---------- */
   const handleDragEnd = async (result) => {
@@ -67,7 +119,7 @@ function UserHome({ currentUser }) {
                   <Draggable>
                     {(provided) => (
                       <li>
-                        <UserGoalCard goal={goal} />
+                        <UserGoalCard goal={goal} onView={handleView} />
                       </li>
                     )}
                   </Draggable>
