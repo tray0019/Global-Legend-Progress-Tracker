38832d48314f85f3afd07603856ab74ab9543f72
tray0019 <tray0019@algonquinlive.com>
2026-01-18 20:25:44 -0500

    good

--- Backend/src/main/java/com/GLPT/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/GLPT/Backend/config/SecurityConfig.java
@@ -3,29 +3,10 @@ package com.GLPT.Backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable()) // disable CSRF for testing / Postman
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/users/oauth-register", "/users/complete-profile/**").permitAll()
-//                        .anyRequest().authenticated()
-//                );
-//
-//        return http.build();
-//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
