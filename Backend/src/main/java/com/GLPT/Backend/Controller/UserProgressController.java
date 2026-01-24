9aab90579434779fb397745f3e8a931791aaa21e
tray0019 <tray0019@algonquinlive.com>
2026-01-24 01:46:22 -0500

    Updated UserProgressController - added user scope method

--- Backend/src/main/java/com/GLPT/Backend/Controller/UserProgressController.java
+++ Backend/src/main/java/com/GLPT/Backend/Controller/UserProgressController.java
@@ -1,10 +1,14 @@
 package com.GLPT.Backend.Controller;
 
 import com.GLPT.Backend.DTO.UserProgressResponse;
+import com.GLPT.Backend.Entity.User;
 import com.GLPT.Backend.Entity.UserProgress;
 import com.GLPT.Backend.Service.UserProgressService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/progress")
@@ -29,6 +33,22 @@ public class UserProgressController {
         );
     }
 
+    private User requireUser(HttpSession session) {
+        User user = (User) session.getAttribute("currentUser");
+
+        if (user == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.UNAUTHORIZED, "Login required");
+        }
+
+        if (!user.isProfileCompleted()) {
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN, "Complete profile first");
+        }
+
+        return user;
+    }
+
     @PostMapping("/xp")
     public ResponseEntity<UserProgress> addXP(@RequestParam int difficulty) {
         progressService.addXP(difficulty);
@@ -36,10 +56,28 @@ public class UserProgressController {
         return ResponseEntity.ok(updated);  // ✅ return updated progress as JSON
     }
 
+    //USER-SCOPE
+    @PostMapping("/user/xp")
+    public ResponseEntity<UserProgress> addXP(@RequestParam int difficulty, HttpSession session) {
+        progressService.addXP(difficulty);
+        User user = requireUser(session);
+        UserProgress updated = progressService.getProgressWithDecayCheckForUser(user);
+        return ResponseEntity.ok(updated);  // ✅ return updated progress as JSON
+    }
+
     @PostMapping("/xp/remove")
     public ResponseEntity<UserProgress> removeXP(@RequestParam int difficulty) {
         progressService.removeXP(difficulty);
         UserProgress updated = progressService.getProgressWithDecayCheck();
+        return ResponseEntity.ok(updated);  // ✅ return updated progress as JSON
+    }
+
+    //User-scope
+    @PostMapping("/user/xp/remove")
+    public ResponseEntity<UserProgress> removeXP(@RequestParam int difficulty, HttpSession session) {
+        User user = requireUser(session);
+        progressService.removeXPForUser(difficulty,user);
+        UserProgress updated = progressService.getProgressWithDecayCheckForUser(user);
         return ResponseEntity.ok(updated);  // ✅ return updated progress as JSON
     }
 
