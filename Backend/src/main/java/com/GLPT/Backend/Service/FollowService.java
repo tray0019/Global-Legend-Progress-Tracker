0dc4f14cbe7ab9bdc94c9dc0d531153ccaa81917
tray0019 <tray0019@algonquinlive.com>
2026-01-28 04:47:53 -0500

    FollowService added unfollowUser and followUser

--- Backend/src/main/java/com/GLPT/Backend/Service/FollowService.java
+++ Backend/src/main/java/com/GLPT/Backend/Service/FollowService.java
@@ -2,6 +2,7 @@ package com.GLPT.Backend.Service;
 
 import com.GLPT.Backend.Entity.User;
 import com.GLPT.Backend.Repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,5 +24,25 @@ public class FollowService {
             me.getFollowing().add(target);
         }
         userRepository.save(me);
+    }
+
+    @Transactional
+    public void followUser(User currentUser, Long targetUserId) {
+        User targetUser = userRepository.findById(targetUserId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (currentUser.getId().equals(targetUserId)) {
+            throw new RuntimeException("You cannot follow yourself!");
+        }
+
+        currentUser.getFollowing().add(targetUser);
+        userRepository.save(currentUser);
+    }
+
+    @Transactional
+    public void unfollowUser(User currentUser, Long targetUserId) {
+        User targetUser = userRepository.findById(targetUserId).orElseThrow();
+        currentUser.getFollowing().remove(targetUser);
+        userRepository.save(currentUser);
     }
 }
