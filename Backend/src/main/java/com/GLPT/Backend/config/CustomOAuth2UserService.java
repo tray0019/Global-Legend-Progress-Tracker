05929b133e57465832244ba53b3f81239de1d49f
tray0019 <tray0019@algonquinlive.com>
2026-01-28 03:33:54 -0500

    Updated CustomOauth2 missing httpsession

--- Backend/src/main/java/com/GLPT/Backend/config/CustomOAuth2UserService.java
+++ Backend/src/main/java/com/GLPT/Backend/config/CustomOAuth2UserService.java
@@ -2,6 +2,7 @@ package com.GLPT.Backend.config;
 
 import com.GLPT.Backend.Entity.User;
 import com.GLPT.Backend.Repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -15,24 +16,27 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private HttpSession httpSession; // Inject the session
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         String email = oAuth2User.getAttribute("email");
 
-        userRepository.findByEmail(email).orElseGet(() -> {
+        User user = userRepository.findByEmail(email).orElseGet(() -> {
             User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setProvider("GOOGLE");
             newUser.setEmail(email);
             newUser.setFirstName(oAuth2User.getAttribute("given_name"));
             newUser.setLastName(oAuth2User.getAttribute("family_name"));
             newUser.setProfileCompleted(false);
-
-            // ADD THESE TWO LINES
-            newUser.setProvider("GOOGLE");
-            newUser.setProviderUserId(oAuth2User.getAttribute("sub")); // Unique ID from Google
-
             return userRepository.save(newUser);
         });
+
+        // THIS IS THE MISSING LINK
+        httpSession.setAttribute("currentUser", user);
 
         return oAuth2User;
     }
